e5003a1ad4d7d9b5dfe674cf3b46488c1676ef5b
guiswp <guilhermeh.t@hotmail.com>
2025-03-19 21:45:51 -0300

    musica gatinho chorando

--- cat-ocean/src/components/App.tsx
+++ cat-ocean/src/components/App.tsx
@@ -1,16 +1,43 @@
-import gatinho from '../assets/gatinho.jpg';
-import '../styles/App.css'
+import { useState, useRef, useEffect } from 'react';
+import gatinhoChorando from '../assets/gatinhochorando.png';
+import musica from '../assets/catlose-to-you.mp3';
+import gatinhoNormal from '../assets/gatinho.png';
+import '../styles/App.css';
+
 const App = () => {
+    const [imagemGato, setImagemGato] = useState(gatinhoNormal);
+    const audioRef = useRef(new Audio(musica));
+
+    const tocarSom = () => {
+        setImagemGato(gatinhoChorando);
+        audioRef.current.currentTime = 0;
+        audioRef.current.play();
+    };
+
+    const pararSom = () => {
+        setImagemGato(gatinhoNormal);
+        audioRef.current.pause();
+        audioRef.current.currentTime = 0;
+    };
+
+    useEffect(() => {
+        audioRef.current.onended = () => setImagemGato(gatinhoNormal);
+
+    },[]);
+
     return (
         <div className='gato'>
-            <img src={gatinho} alt="gatinho ocean" width={300} className='imagemgato'/>
-            <div className='buttom1'>
-                <button>
-                    MEOW
-                </button>
-            </div>
+            <img
+                src={imagemGato} 
+                alt="gatinho ocean" 
+                width={300} 
+                className='imagemgato'
+                onMouseEnter={pararSom}
+                onMouseLeave={tocarSom}
+            
+            />
         </div>
-    )
+    );
 };
 
 export default App;
