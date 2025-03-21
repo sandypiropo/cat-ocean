dab74b55d59c235a59a289dbd01ece29f39ce2a3
guiswp <guilhermeh.t@hotmail.com>
2025-03-21 19:20:34 -0300

    backgrounds novos e gato centralizado

--- cat-ocean/src/components/App.tsx
+++ cat-ocean/src/components/App.tsx
@@ -1,3 +1,4 @@
+
 import { useState, useRef, useEffect } from 'react';
 import gatinhoChorando from '../assets/gatinhochorando.png';
 import musica from '../assets/catlose-to-you.mp3';
@@ -5,6 +6,7 @@ import gatinhoNormal from '../assets/gatinho.png';
 import '../styles/App.css';
 
 const App = () => {
+    const [background, setBackground] = useState('#ffffff');
     const [imagemGato, setImagemGato] = useState(gatinhoNormal);
     const audioRef = useRef(new Audio(musica));
 
@@ -26,16 +28,23 @@ const App = () => {
     },[]);
 
     return (
-        <div className='gato'>
-            <img
-                src={imagemGato} 
-                alt="gatinho ocean" 
-                width={300} 
-                className='imagemgato'
-                onMouseEnter={pararSom}
-                onMouseLeave={tocarSom}
-            
-            />
+        <div className='container' style={{ backgroundColor: background }}>
+            <div className='theme-selector'>
+            <div className='theme-box orange' onClick={() => setBackground('#f37424')}></div>
+            <div className='theme-box white' onClick={() => setBackground('#ffffff')}></div>
+            <div className='theme-box pink' onClick={() => setBackground('#ffc0cb')}></div>
+            <div className='theme-box green' onClick={() => setBackground('#0b490bd7')}></div>
+        </div>
+        
+            <div className='gato'>
+                <img
+                    src={imagemGato}
+                    alt="gatinho ocean"
+                    className='imagemgato'
+                    onMouseEnter={pararSom}
+                    onMouseLeave={tocarSom} 
+                />
+            </div>
         </div>
     );
 };
